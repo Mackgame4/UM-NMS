11635b5233ea8a096f0d0a3bda3b3bb6e7789055
Mack <42896437+Mackgame4@users.noreply.github.com>
2024-10-25 12:03:36 +0100

    Update NMS_Server.py

--- server/NMS_Server.py
+++ server/NMS_Server.py
@@ -30,17 +30,24 @@ class NMS_Server:
             threading.Thread(target=self.handle_connection, args=(conn, client_id)).start()
 
     def handle_connection(self, conn, client_id):
-        conn.send(encode_message(f"Welcome, your client ID is {client_id}"))
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print(f"Client {client_id} disconnected")
-                break
-            message = decode_message(data)
-            print(f"Received from Client {client_id}: {message}")
-            response = f"Received by Server: {message}"
-            conn.send(encode_message(response))
-        conn.close()
+        try:
+            conn.send(encode_message(f"Welcome, your client ID is {client_id}"))
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print(f"Client {client_id} disconnected")
+                    break
+                message = decode_message(data)
+                print(f"Received from Client {client_id}: {message}")
+                response = f"Received by Server: {message}"
+                conn.send(encode_message(response))
+        except ConnectionResetError:
+            print(f"Client {client_id} forcibly disconnected")
+        finally:
+            # Ensure the connection is closed and notify of disconnection
+            conn.close()
+            self.connectedAgents = [(c, id) for c, id in self.connectedAgents if id != client_id]
+            print(f"Client {client_id} disconnected")
 
     def close(self):
         self.socket_tcp.close()
